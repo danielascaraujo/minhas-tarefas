a12dfd4716b309e99b44474a553eef9619a6ba03
Daniela Araujo <daniela.scaraujo@outlook.com>
2024-11-15 13:25:33 -0300

    configurações diversa

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da tarefa</S.Titulo>
-    <S.Tag>importantes</S.Tag>
-    <S.Tag>pendentes</S.Tag>
-    <S.Descricao />
-    <S.BarraAcores>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcores>
-  </S.Card>
-)
+import * as enums from '../../utils/enums/Tarefa'
+
+type Props = {
+  titulo: string
+  prioridade: enums.Prioridade
+  status: enums.Status
+  descricao: string
+}
+
+const Tarefa = ({ descricao, prioridade, status, titulo }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag parametro="prioridade" prioridade={prioridade}>
+        {prioridade}
+      </S.Tag>
+      <S.Tag parametro="status" status={status}>
+        {status}
+      </S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcores>
+        {estaEditando ? (
+          <>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelarRemover onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCancelarRemover>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.BotaoCancelarRemover>Remover</S.BotaoCancelarRemover>
+          </>
+        )}
+      </S.BarraAcores>
+    </S.Card>
+  )
+}
 
 export default Tarefa
